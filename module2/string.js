afc4eb6d83ef90030f436a4e27a54ee566e9db52
99ajay <tigersis1995@gmail.com>
2022-03-06 00:02:36 +0530

    string_js

---
+++ module2/string.js
@@ -0,0 +1,46 @@
+let str="i am a string";
+console.log(str);
+
+//length of the string
+
+let len=str.length;
+console.log(len);
+
+//sliced method
+//extract part of the string
+let slicedarr=str.slice(2,8);
+console.log(slicedarr);
+
+//replace method
+let replacedstr=str.replace("am","was");
+console.log(replacedstr);
+console.log(str);
+
+
+let uppercase=str.toUpperCase();
+console.log(uppercase);
+
+let lowercase=str.toLowerCase();
+console.log(lowercase);
+
+//concate
+
+let firstname="ajay verma";
+let lastname="kashi";
+//let fullname=firstname+lastname;
+//or
+let fullname=firstname.concat(lastname);
+console.log(fullname);
+
+
+//split->return arry in javascript;
+let splitted=str.split(" ");
+console.log(splitted);
+
+//Home work to do trim
+//go to the link and do 40 question.......https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#basic-javascript
+
+
+
+
+
