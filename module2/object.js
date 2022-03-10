764e92aa818ba3cf0d9eb0b402b17ce339b5b336
99ajay <tigersis1995@gmail.com>
2022-03-10 20:39:55 +0530

    object

--- module2/object.js
+++ module2/object.js
@@ -9,4 +9,28 @@ let objajay={
     lastname : 'verma'
 };
 console.log(objajay);
+let forest={
+    forest_name:chehat_gadh,
+    mo_nu:8577871550,
+    age_of_forest:897,
+    jasprit:'heny_peny'
+}
+console.log(forest);
+
+
+let captionamerica={
+    name:'steve',//key:string
+    age:67,//key:number
+    friend : ['natasha','thor','hulk','vision','vanda'],  //key:array
+    //key:object
+    address: {
+        city:"quens",
+        state: 'uttar pradesh',
+    },
+    //key: function
+    sayhii:function()
+    {
+        console.log('caption america says hii to me sir');
+    }
+}
 
