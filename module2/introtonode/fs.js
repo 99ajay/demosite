1373ba909ddde9d07b60e55da50aeed10804e9db
99ajay <tigersis1995@gmail.com>
2022-03-10 23:57:34 +0530

    calculator

--- module2/introtonode/fs.js
+++ module2/introtonode/fs.js
@@ -15,7 +15,7 @@ let filePath=path.join(__dirname,"file.txt");
 fs.writeFileSync(filePath,"hello i am a ajay verma");//create file if file does not exit inn the folder and override if it is already present in the folder 
 
 //r read
-console.log("before update : "); 
+console.log("before update : ");
 let content=fs.readFileSync(filePath,'utf-8');
 console.log(content);
 
@@ -28,4 +28,4 @@ console.log(fs.readFileSync(filePath,'utf-8'));
 
 
 //D delete
-fs.unlinkSync(filePath);
+//fs.unlinkSync(filePath);
---
+++ module2/introtonode/module/testmodule.js
@@ -0,0 +1,6 @@
+let calc=require("./calculator.js");
+
+console.log(calc.addition(46,24));
+console.log(calc.subtraction(46,24));
+console.log(calc.multiply(46,24));
+console.log(calc.division(48,24));
