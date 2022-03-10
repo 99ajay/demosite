f925394d43e735f432d96f28ef0b9db43f01c566
99ajay <tigersis1995@gmail.com>
2022-03-10 23:32:52 +0530

    CRUD COMPLETED

--- module2/introtonode/fs.js
+++ module2/introtonode/fs.js
@@ -1,3 +1,8 @@
+//CRUD OPERATION 
+
+
+
+
 let fs=require("fs");
 //console.log(fs);
 
@@ -6,5 +11,21 @@ let path=require("path");
 let filePath=path.join(__dirname,"file.txt");
 //console.log(filePath);
 
-fs.writeFileSync(filePath,"hello i am a ajay verma");
-//fs.writeFileSync()
+//c create
+fs.writeFileSync(filePath,"hello i am a ajay verma");//create file if file does not exit inn the folder and override if it is already present in the folder 
+
+//r read
+console.log("before update : "); 
+let content=fs.readFileSync(filePath,'utf-8');
+console.log(content);
+
+//try to write after the file not replace the file 
+
+//u update
+fs.appendFileSync(filePath," newly added content");
+console.log("after update : ");
+console.log(fs.readFileSync(filePath,'utf-8'));
+
+
+//D delete
+fs.unlinkSync(filePath);
