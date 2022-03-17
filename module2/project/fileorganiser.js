684a818709a31d84fe0321aaa972416dc6d342bb
99ajay <tigersis1995@gmail.com>
2022-03-17 23:39:37 +0530

    read_dir

--- module2/project/fileorganiser.js
+++ module2/project/fileorganiser.js
@@ -33,7 +33,15 @@ if(folderExists)
     for(let i=0;i<files.length;i++){
         let ext = path.extname(files[i]);
         let nameOfFolder = giveFolderName(ext);
-        console.log("Ext--",ext,"Folder--",nameOfFolder);
+        //console.log("Ext--",ext,"Folder--",nameOfFolder);
+        let pathOfFolder = path.join(folderPath,nameOfFolder); 
+        let exist = fs.existsSync(pathOfFolder);
+        if(exist){
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }else{
+            fs.mkdirSync(pathOfFolder);
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }
     }
 }
 
@@ -52,4 +60,10 @@ function giveFolderName(ext){
         }
     }
     return 'Others'
+}
+function moveFile(folderPath,pathOfFolder,fileName){
+    let sourcePath = path.join(folderPath,fileName);
+    let destinationPath = path.join(pathOfFolder,fileName);
+    fs.copyFileSync(sourcePath,destinationPath);
+    fs.unlinkSync(sourcePath);
 }
