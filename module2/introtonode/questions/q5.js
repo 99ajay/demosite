74bb7099661d8b4d7f4b3f6bea35bc055dd2e7b6
99ajay <tigersis1995@gmail.com>
2022-04-07 15:47:43 +0530

    q5 completed

--- module2/introtonode/questions/q5.js
+++ module2/introtonode/questions/q5.js
@@ -1,1 +1,15 @@
-//move a file
+//move a file\
+
+//there is no direct way to move the file from one directory to another directory method is that first we copy the file from one directory to where we have to move paste there and then delete the sourcepath of that file from there
+let fs=require("fs");
+let path=require("path");
+
+console.log(__dirname);
+let sourcepath=path.join(__dirname,"file11.txt");
+
+let destinationpath=path.join(__dirname,"..","..","file11.txt");
+console.log(sourcepath);
+console.log(destinationpath);
+
+fs.copyFileSync(sourcepath,destinationpath);
+fs.unlinkSync(sourcepath);///deleting after moving the file
