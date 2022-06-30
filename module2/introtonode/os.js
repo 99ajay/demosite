9ff16c81bcecac890a8609bafb050c67ba6dd0d2
99ajay <tigersis1995@gmail.com>
2022-06-30 22:56:03 +0530

    revision

--- module2/introtonode/os.js
+++ module2/introtonode/os.js
@@ -1,6 +1,13 @@
+// let os=require('os');
+//  console.log(os);
+//  console.log(os.hostname());
+// console.log(os.arch());
+// console.log(os.networkInterfaces());
+// console.log(os.userInfo());
+// console.log(os.version());
 let os=require('os');
- console.log(os);
- console.log(os.hostname());
+//console.log(os);
+console.log(os.hostname());
 console.log(os.arch());
 console.log(os.networkInterfaces());
 console.log(os.userInfo());
