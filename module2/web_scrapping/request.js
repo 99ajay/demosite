e7601016abb3bd8232f1b650b80f26c9fe2e66ba
99ajay <tigersis1995@gmail.com>
2022-07-06 11:43:20 +0530

    request

--- module2/web_scrapping/request.js
+++ module2/web_scrapping/request.js
@@ -1,10 +1,28 @@
 //site---->https://www.npmjs.com/package/request
-const request = require('request');
-request('http://www.google.com', cb);
-function cb(error, response, body) {
-if(error)   
-     console.error('error:', error); // Print the error if one occurred
+const request = require('request'); //gona use request command from package 
+
+request('http://www.google.com', function (error, response, body) {
+     if(error)
+     {
+         console.error('error:', error); // Print the error if one occurred
+     }
+     else
+     {
+         console.log('body:', body); // Print the HTML for the Google homepage.
+     }
+});
+
+
+
+
+
+
+// request('http://www.google.com', cb);
+
+// function cb(error, response , body) { //
+// if(error)   
+//      console.error('error:', error); // Print the error if one occurred
  
-else    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-     console.log('body:', body); // Print the HTML for the Google homepage.
-};
+// else    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//      console.log('body:', body); // Print the HTML for the Google homepage.
+// }
