4327fdd99710d6bc0861ba692f77aac3b37c8b3c
99ajay <tigersis1995@gmail.com>
2022-07-18 00:22:40 +0530

    webscrapping

--- module2/web_scrapping/callback.js
+++ module2/web_scrapping/callback.js
@@ -1,8 +1,19 @@
 const fs = require("fs");
 
+// console.log("Before ");
+
+// let content=fs.readFileSync("file.txt",);
+// console.log(content+" ");
+
+// console.log("After");
+
+
+
+
 console.log("Before ");
 
-fs.readFile("file.txt",cb);
+fs.readFile("file.txt",cb);  //remove sync and pass a callback to a function to skip current task to which callback is made in this way that task will run when all code run 
+
 //console.log(content+"");
 
 function cb(error,data){
--- module2/web_scrapping/request.js
+++ module2/web_scrapping/request.js
@@ -1,16 +1,27 @@
 //site---->https://www.npmjs.com/package/request
 const request = require('request'); //gona use request command from package 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const link="https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard";
+request(link,cb);
 
-request('http://www.google.com', function (error, response, body) {
+     function cb(error, response, html) {
      if(error)
      {
          console.error('error:', error); // Print the error if one occurred
      }
      else
-     {
-         console.log('body:', body); // Print the HTML for the Google homepage.
+     {   
+         const dom=new JSDOM(html);
+       //  const document=dom.window.document.querySelector("p").textContent;
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+       // console.log(document);
+       const document=dom.window.document;
+       let teamsName=document.querySelectorAll(".ci-team-score.ds-flex.ds-justify-between.ds-items-center.ds-text-typo-title.ds-mb-2 .ds-text-tight-l.ds-font-bold");
+       console.log(teamsName[0].textContent);
+       console.log(teamsName[1].textContent);
      }
-});
+};
 
 
 
