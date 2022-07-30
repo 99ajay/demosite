29c4c1305be81a1ef82b25f6d6e7435838bcab52
99ajay <tigersis1995@gmail.com>
2022-07-30 18:37:32 +0530

    seriallypromises

--- module2/automation/raw.js
+++ module2/automation/raw.js
@@ -1,15 +1,24 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');  //require phase
 
-let browserPromise = puppeteer.launch({ headless: false });
+let browserPromise = puppeteer.launch({ headless: false });  //it is doing promise to launch the browser //if headless is true then our browser is opened but hat will be not visible to us 
 
-browserPromise.then(function(browser){
+// browserPromise.then(function(browser){
+//     console.log("Browser is opened");
+//     let pagePromise = browser.newPage();
+//     return pagePromise;
+// }).then(function(page){
+//     console.log("page is opened");
+//     let urlPromise = page.goto('https://www.google.com/');
+//     return urlPromise
+// }).then(function(){
+//     console.log("google is opened");
+
+// })
+
+let browserPromise=puppeteer.launch({headless : false});
+browserPromise.then(function(browserInstance) //if promise is complete then pass browser instance 
+{
     console.log("Browser is opened");
-    let pagePromise = browser.newPage();
-    return pagePromise;
-}).then(function(page){
-    console.log("page is opened");
-    let urlPromise = page.goto('https://www.google.com/');
-    return urlPromise
-}).then(function(){
-    console.log("google is opened");
+    let pagePromise=browserInstance.newPage(); //to open for the new page in the browser 
 })
+ 
