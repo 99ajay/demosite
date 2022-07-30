c3e2d5bf8ad69e08e1366eddeed3df53800587a6
99ajay <tigersis1995@gmail.com>
2022-07-30 15:12:18 +0530

    promises start

--- module2/automation/promise.js
+++ module2/automation/promise.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 console.log("Before");
 let f1KaPromise = fs.promises.readFile("f1.txt");
+let f2KaPromise = fs.promises.readFile("f2.txt");
+
 
 
 f1KaPromise.then(function(data){
@@ -9,6 +11,14 @@ f1KaPromise.then(function(data){
 })
 
 f1KaPromise.catch(function(error){
+    console.log(error);
+})
+
+f2KaPromise.then(function(data){
+    console.log(data+"");
+})
+
+f2KaPromise.catch(function(error){
     console.log(error);
 })
 
