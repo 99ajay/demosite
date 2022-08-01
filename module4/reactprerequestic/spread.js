44a0410356eadec6ce8893c35a6b7b8d3da89e8c
99ajay <tigersis1995@gmail.com>
2022-08-01 15:31:24 +0530

    rp

--- module4/reactprerequestic/spread.js
+++ module4/reactprerequestic/spread.js
@@ -11,36 +11,36 @@ console.log(arr2);
 
 //spread operator with objects;
 
-let obj={
-    name:"adam",address:{country:"USA",state:{
-        stateName:"India",pincode:123456
-    }}}
+// let obj={
+//     name:"adam",address:{country:"USA",state:{
+//         stateName:"India",pincode:123456
+//     }}}
 
-//shallow copy
-let obj2={...obj};
+// //shallow copy
+// let obj2={...obj};
 
-obj2.name="ajay";
+// obj2.name="ajay";
+// // console.log(obj.name);
+
+// // console.log(obj2.name);
+
+// // obj2.address=
+// // {
+// //     country:"india",state:"delhi",pincode:12345
+// // }
+
+// obj2.address.country="india";
+
 // console.log(obj.name);
-
+// console.log(obj.address);
+// console.log("##################");
 // console.log(obj2.name);
+// console.log(obj2.address);
 
-// obj2.address=
-// {
-//     country:"india",state:"delhi",pincode:12345
-// }
+// //deep copy
 
-obj2.address.country="india";
+// let obj3=JSON.parse(JSON.stringify(obj))
 
-console.log(obj.name);
-console.log(obj.address);
-console.log("##################");
-console.log(obj2.name);
-console.log(obj2.address);
-
-//deep copy
-
-let obj3=JSON.parse(JSON.stringify(obj))
-
-obj3.address.country="japan";
-console.log(obj.address.country);
-console.log(obj3.address.country);
+// obj3.address.country="japan";
+// console.log(obj.address.country);
+// console.log(obj3.address.country);
