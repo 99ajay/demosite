195d957977a4ac7277e77c22586dadd3336eab0f
99ajay <tigersis1995@gmail.com>
2022-08-09 22:05:30 +0530

    module3 started assessesment added

--- freecodecamp/tempCodeRunnerFile.cpp
+++ freecodecamp/tempCodeRunnerFile.cpp
@@ -1,21 +1,67 @@
+ #include <bits/stdc++.h>
 
-      string solution(string str) {
-        map<int, string> map;
-        string temp;
-		
-        for(auto i:str){
-            if(isdigit(i))
-                map[i] = temp;
-            else if(i == ' ')
-                temp = "";
-            else
-                temp += i;
-        }
-        string ans;
-        for(auto i:map)
-        {
-            ans += i.second + " ";
-        }
-        ans.pop_back();  
-        return ans;
-    }
+using namespace std;
+
+#define int long long
+
+#define endl "\n"
+
+vector<int> adj[10001];
+
+int32_t main(){
+
+int n; cin>>n;
+
+for(int i=0;i<n-1;i++){
+
+int x,y; cin>>x>>y;
+
+adj[x].push_back(y);
+
+adj[y].push_back(x);
+
+}
+
+int k; cin>>k;
+
+vector<int> p(k);
+
+int lvl[n+1]={0},visit[n+1]={0};;
+
+for(int i=0;i<k;i++) cin>>p[i],visit[p[i]]=1;
+
+queue<int> q;
+
+int ma=0;
+
+q.push(1);
+
+while(!q.empty()){
+
+int x=q.front();
+
+q.pop();
+
+visit[x]=1;
+
+for(auto i:adj[x]){
+
+if(visit[i]==0){
+
+q.push(i);
+
+lvl[i]+=lvl[x]+1;
+
+visit[i]=1;
+
+}
+
+}
+
+}
+
+for(int i=1;i<=n;i++) ma=max(ma,lvl[i]);
+
+cout<<ma<<endl;
+
+}
