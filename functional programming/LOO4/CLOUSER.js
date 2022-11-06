2510d8ed543ec4f8aebc36c9d0eef510324c78a4
99ajay <tigersis1995@gmail.com>
2022-11-06 13:09:19 +0530

    clouser

---
+++ functional programming/LOO4/CLOUSER.js
@@ -0,0 +1,25 @@
+
+// function calculate(a,b)
+// {
+//     let result =a+b;
+//     return result;
+// }
+
+// console.log(calculate(2,3));
+
+// console.log(result);//not possible to access outside the function rule of javascript either declare by var or let does not matter 
+
+
+function add()
+{
+    let a=4;
+
+    function addchild()
+    {
+        console.log(a+5);
+    }
+    addchild();
+}
+add();
+
+
