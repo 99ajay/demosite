9ddc66d85a7ed197d78c2cc3a551dc2df631b0c6
AJAY VERMA <tigersis1995@gmail.com>
2022-12-06 15:22:11 +0530

    Click event and function

--- REACT/blog/src/App.js
+++ REACT/blog/src/App.js
@@ -2,14 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import User from './User';
 import User1 from './User1';
+// alert("hello");
 
 function App() {
+  let data="petar parker";
+  function apple(){
+    data="thanos";
+    alert(data);
+  }
   return (
     <div className="App">
       <h1>JSX</h1>
-      <User/>
+      {/* <button onClick={apple()}>click me</button> //call atonce */}
+      <button onClick={apple}>click me</button> 
 
-      <User1 />
+      <button onClick={()=>apple()}>click me</button>
+
+      <button onClick={()=>alert("ajay verma")}>click me</button>
     </div>
   );
 }
