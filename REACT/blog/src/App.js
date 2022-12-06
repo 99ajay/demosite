6a38dee53bfed4a5b1b5df4b6c82d6e4497eb8d5
AJAY VERMA <tigersis1995@gmail.com>
2022-12-06 18:09:47 +0530

    state

--- REACT/blog/src/App.js
+++ REACT/blog/src/App.js
@@ -2,23 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import User from './User';
 import User1 from './User1';
+import {useState} from "react";
 // alert("hello");
 
 function App() {
-  let data="petar parker";
-  function apple(){
-    data="thanos";
-    alert(data);
+  // let data="petar parker";
+  const [data,setData]=useState(0);
+  function updateData(){
+    // data="thanos";
+    // alert(data);
+    setData(data+1);
   }
   return (
     <div className="App">
-      <h1>JSX</h1>
+      {/* <h1>JSX</h1>
       {/* <button onClick={apple()}>click me</button> //call atonce */}
-      <button onClick={apple}>click me</button> 
+      {/* <button onClick={apple}>click me</button> 
 
       <button onClick={()=>apple()}>click me</button>
 
-      <button onClick={()=>alert("ajay verma")}>click me</button>
+      <button onClick={()=>alert("ajay verma")}>click me</button> */}
+      <h1>{data}</h1>
+      <button onClick={updateData}>Update Data</button>
+
+    
+
     </div>
   );
 }
