8bb5579e072d3d0a5f01dd531110696e57b9c3ee
AJAY VERMA <tigersis1995@gmail.com>
2022-12-08 16:38:23 +0530

    props functional change value on click event

--- REACT/blog/src/App.js
+++ REACT/blog/src/App.js
@@ -1,21 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import Student from './Student';
  
 
  
    function App()
    {
+     const [data,setState]=useState("ajay verma");
     return(
     <div className="App"> 
      <h1>props in react</h1>
-     <Student name={"ajay"} mail={"ajayverma@gmail.com"} address={{village :'mahangipur',post : "raisipur"}}/>
-     <Student name={"ajay"} mail={"ajayverma@gmail.com"} address={{village :'mahangipur',post : "raisipur"}}/>
-     <Student name={"ajay"} mail={"ajayverma@gmail.com"} address={{village :'mahangipur',post : "raisipur"}}/>
-     <Student name={"ajay"} mail={"ajayverma@gmail.com"} address={{village :'mahangipur',post : "raisipur"}}/>
-
-     <Student name={"ajay"} mail={"ajayverma@gmail.com"} address={{village :'mahangipur',post : "raisipur"}}/>
+     <Student name={data} />
+     <button onClick={()=>{setState("Bholuu Patel")}}>update name</button>
+     
     </div>
     );
    }
--- REACT/blog/src/Student.js
+++ REACT/blog/src/Student.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 function Student(props){
+  
+
     return(
         <div style={{backgroundColor : "blue",margin : 10}}>
             <h1>Hello {props.name}</h1>
-            <h2>Email: {props.mail}</h2>
-            <h3>Address : {props.address.village} , {props.address .post}</h3>
+            
         </div>
     );
 }
