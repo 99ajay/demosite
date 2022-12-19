6ef3c33d56b6bf1d0d68fd191e18e3a724a49e92
AJAY VERMA <tigersis1995@gmail.com>
2022-12-19 21:38:26 +0530

    pagination added

--- module4/react/movies-app/src/components/Movielist.js
+++ module4/react/movies-app/src/components/Movielist.js
@@ -5,7 +5,8 @@ class Movielist extends Component {
     constructor() {
         super();
         this.state = {
-            hover: ""
+            hover: "",
+            pArr : [1]
         };
     }
     render() {
@@ -36,9 +37,9 @@ class Movielist extends Component {
                     <nav aria-label="Page navigation example">
                         <ul className="pagination">
                             <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-                            <li className="page-item"><a className="page-link" href="#">1</a></li>
-                            <li className="page-item"><a className="page-link" href="#">2</a></li>
-                            <li className="page-item"><a className="page-link" href="#">3</a></li>
+                            {this.state.pArr.map(()=>  <li className="page-item"><a className="page-link" href="#">1</a></li>)}
+                          
+                             
                             <li className="page-item"><a className="page-link" href="#">Next</a></li>
                         </ul>
                     </nav>
